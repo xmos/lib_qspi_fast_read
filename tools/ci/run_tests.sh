3d6ea971d9c254da3edad7e55b40bb873f5e7159
johnshaferXMOS <johnshafer@xmos.com>
2023-03-30 15:45:39 -0400

    directory mishandling after reversion

--- tools/ci/run_tests.sh
+++ tools/ci/run_tests.sh
@@ -33,7 +33,10 @@ fi
 # discern repository root
 LIB_QSPI_FAST_READ_ROOT=`git rev-parse --show-toplevel`
 
+START_DIR=$PWD
+cd ${LIB_QSPI_FAST_READ_ROOT}/build
 make run_test_general
 make run_test_resource_reuse
 make run_test_stress_600
 make run_test_stress_800
+cd ${START_DIR}
